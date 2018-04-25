a5d0dd9d10b04d1e06d10c9cdc1fee2c2ecf65a2
Cristian Perez <creshandkesh@gmail.com>
2018-04-24 22:17:08 -0500

    update dependencies, update db

--- db.sql
+++ db.sql
@@ -24,11 +24,6 @@ insert into employees(name,paternalName,maternalName,birthday,email, userId) VAL
 insert into employees(name,paternalName,maternalName,birthday,email, userId) VALUES ('Richy','Gallegos','Gallegos','2008-7-04','dto@opal.com',2);
 insert into employees(name,paternalName,maternalName,birthday,email, userId) VALUES ('Cristian','Perez',':v','2008-7-04','mochilas@opal.com',3);
 
-drop table if exists token;
-create TABLE token(
-  id serial primary key not null,
-  token VARCHAR(200) not null
-);
 
 drop table if exists customers;
 create table customers (
@@ -44,22 +39,62 @@ drop table if exists products;
 create table products(
   id serial primary key not null,
   name varchar(150) not null ,
-  price numeric not null
+  description varchar(150) not null,
+  image bytea
 );
 
-insert into products(name, price) values ('Dona de chocolate',4);
-insert into products(name, price) values ('Dona de fresa',4);
-insert into products(name, price) values ('Dona de coco',5);
-insert into products(name, price) values ('Dona de nuez',5);
-insert into products(name, price) values ('Dona glaseada',6);
-insert into products(name, price) values ('Dona de crema',10);
-insert into products(name, price) values ('Donita',2);
-insert into products(name, price) values ('Donota',12);
+insert into products(name, description) values ('Dona de chocolate','La dona llena de negro chocolate');
+insert into products(name, description) values ('Dona de fresa','Dona llena de fresa');
+insert into products(name, description) values ('Dona de coco','Dona con glaseado y pedazos de coco');
+insert into products(name, description) values ('Dona de nuez','Dona con glaseado de nuez');
+insert into products(name, description) values ('Dona glaseada','Una dona bien glaseada');
+insert into products(name, description) values ('Dona de crema','Dona con mucha crema');
+insert into products(name, description) values ('Donita','mini donita');
+insert into products(name, description) values ('Donota','Una donota para los que tienen hambre');
+
+drop table if exists productPrices;
+create table productPrices(
+  productId int references products(id),
+  date date,
+  price numeric
+);
+
+create index pricesid on productPrices (productId) ;
+
+insert into productPrices(productId, date, price) values (1,now(),4);
+insert into productPrices(productId, date, price) values (2,now(),4);
+insert into productPrices(productId, date, price) values (3,now(),5);
+insert into productPrices(productId, date, price) values (4,now(),5);
+insert into productPrices(productId, date, price) values (5,now(),6);
+insert into productPrices(productId, date, price) values (6,now(),10);
+insert into productPrices(productId, date, price) values (7,now(),2);
+insert into productPrices(productId, date, price) values (8,now(),12);
+
+-- consulta para obtener el precio de las ordenes
+select price, date from productPrices where productId = 1 and
+                                                      date between '23/04/2018' and '25/04/2018';
+
+drop table if exists stock;
+create table stock(
+  productId int references products(id) primary key ,
+  quantity int
+);
+
+insert into stock(productId, quantity) values (1,10);
+insert into stock(productId, quantity) values (2,10);
+insert into stock(productId, quantity) values (3,10);
+insert into stock(productId, quantity) values (4,10);
+insert into stock(productId, quantity) values (5,10);
+insert into stock(productId, quantity) values (6,10);
+insert into stock(productId, quantity) values (7,10);
+insert into stock(productId, quantity) values (8,10);
 
 drop table if exists orders;
 create table orders(
   id serial primary key not null,
-  status boolean default false
+  customerId int references customers(id),
+  status boolean default false,
+  orderDate date
 );
 
 drop table if exists customer_order;
